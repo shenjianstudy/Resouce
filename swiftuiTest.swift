d53c40989e28cee937fceb70d584c92e2daf7189
shenjianstudy <shenjianstudy@126.com>
2024-07-31 10:24:01 +0800

    Update and rename msg-list.json to swiftuiTest.swift

---
+++ swiftuiTest.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
